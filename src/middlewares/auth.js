6c2453f72c76bb4c40f7867e17fd6346aac2a624
DouglasKomori <douglassatoshi@gmail.com>
2024-12-11 22:58:09 -0300

    omg

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,9 @@
 const isAuthenticated = (req, res, next) => {
-  if (req.session.user) {
+  if (req.session.cookie) {
     next();
   } else {
     return res.status(403).json({ errors: ['not authenticated'] });
   }
 };
 
-module.exports = { isAuthenticated }
+module.exports = { isAuthenticated }
